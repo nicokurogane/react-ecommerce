43781e50e874306f6b697de3b4a5dd435cfafbaa
Nicolas Martinez <fyedream@gmail.com>
2019-08-09 01:18:39 -0600

    Added cart counter functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import MessagePopUp from "./message-popup/MessagePopUp";
+
+import cartImg from "../assets/cart.png";
 
 import "./App.css";
 
-function App(props) {
-  return (
-    <header className="App">
-      <header className="app-header">
-        <span>Turing Ecommerce</span>
-        <div className="links-container">
-          <Link to="/" className="link-header">
-            Home
-          </Link>
-          <Link to="/" className="link-header">
-            Login
-          </Link>
-          <Link to="/cart/details" className="link-header">
-            My Cart
-          </Link>
-        </div>
+class ConnectedApp extends React.Component {
+  render() {
+    return (
+      <header className="App">
+        <header className="app-header">
+          <span>Turing Ecommerce</span>
+          <div className="links-container">
+            <Link to="/" className="link-header">
+              Home
+            </Link>
+            <Link to="/" className="link-header">
+              Login
+            </Link>
+            <Link to="/cart/details" className="link-header">
+              <img src={cartImg} alt="cart" />
+              <span
+                className="cart-counter"
+                style={{
+                  display: this.props.cartCounter > 0 ? "block" : "none"
+                }}
+              >
+                {this.props.cartCounter}
+              </span>
+            </Link>
+          </div>
+        </header>
+        <div className="container">{this.props.children}</div>
+        <MessagePopUp />
       </header>
-      <div className="container">{props.children}</div>
-      <MessagePopUp />
-    </header>
-  );
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    cartCounter: state.cart.list.length
+  };
+};
+
+const App = connect(
+  mapStateToProps,
+  {}
+)(ConnectedApp);
 
 export default App;
